841c08f1304d0a265413e98afda61689533dcce1
Fernando Candiani <candianirfernando@gmail.com>
2024-07-28 15:58:50 -0300

    Contrução de endpoints

--- src/Journey.Api/obj/Debug/net8.0/Journey.Api.AssemblyInfo.cs
+++ src/Journey.Api/obj/Debug/net8.0/Journey.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Journey.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f5894c709924fad2cb022a1668dd2fd21aaac76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Journey.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Journey.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
